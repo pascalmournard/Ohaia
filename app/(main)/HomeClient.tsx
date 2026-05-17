2118d362a6b351363289aac2e1fe53205d6d624d
pascalmournard <pascalmournard@hotmail.fr>
2026-05-17 18:43:56 +0200

    components/layout/Header.tsx et app/(main)/HomeClient.tsx

--- app/(main)/HomeClient.tsx
+++ app/(main)/HomeClient.tsx
@@ -103,7 +103,7 @@ const MODE_ICONS = {
     </svg>
   ),
   DON: (
-    <svg width="18" height="18" viewBox="0 0 20 20" fill="none" stroke="currentColor" strokeWidth="1.5">
+    <svg width="18" height="18" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="1.5">
       <path d="M20.84 4.61a5.5 5.5 0 0 0-7.78 0L12 5.67l-1.06-1.06a5.5 5.5 0 0 0-7.78 7.78L12 21.23l8.84-8.84a5.5 5.5 0 0 0 0-7.78z" />
     </svg>
   ),
